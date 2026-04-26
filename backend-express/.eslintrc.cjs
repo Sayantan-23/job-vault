3ec99311ac575e550400b16edaf62e9867e370fc
sayantan-23 <sayantan358@gmail.com>
2026-04-26 22:56:25 +0530

    chore(backend-express): add TypeScript, ESLint, Prettier, Vitest configs and align deps

---
+++ backend-express/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      { assertionStyle: 'as', objectLiteralTypeAssertions: 'never' },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'no-console': 'error',
+    'no-default-export': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+}
